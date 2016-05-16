f2a202062c3b5227736fc441ba08305f6997858f
Adam Schubert <adam.schubert@sg1-game.net>
2016-05-16 22:26:11 +0200

    Prepare testing

---
+++ test/FirstTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+
+
+class FirstTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function someTest()
+    {
+
+    }
+}
